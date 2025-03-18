eddaadd4c66ccec5f61ba4830399d4ee5260e666
omer_yavas <omer.yavas@solmaz.com>
2025-03-18 16:48:30 +0300

    add JSON parsing middleware to the application

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
-const port = 3000;
-
 const hotelRouter = require("./routes/hotelRoutes");
+
+// Middleware to parse JSON
+app.use(express.json());
+
 //  ROUTES
 app.use("/api/v1/hotels", hotelRouter);
 
