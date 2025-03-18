6274a147eebc8a5f53b4f17f5364fbc9b98cd5d4
omer_yavas <omer.yavas@solmaz.com>
2025-03-18 12:12:00 +0300

    add hotel routes to the main application

--- app.js
+++ app.js
@@ -2,4 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const hotelRouter = require("./routes/hotelRoutes");
+//  ROUTES
+app.use("/api/v1/hotels", hotelRouter);
+
 module.exports = app;
